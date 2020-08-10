cf984cd455f686706dafc028b1718cd23c54acde
Glitch (hello-webpage) <none>
2020-08-10 23:11:22 +0000

    🏹🏎 Checkpoint ./script.js:11507695/12125 ./index.html:11507695/882 ./style.css:11507695/168

--- script.js
+++ script.js
@@ -1,5 +1,61 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var BootScene = new Phaser.Class({
+  Extends: Phaser.Scene,
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+  initialize: function BootScene() {
+    Phaser.Scene.call(this, { key: "BootScene" });
+  },
+
+  preload: function() {
+    // map tiles
+    this.load.image("tiles", "assets/map/spritesheet.png");
+
+    // map in json format
+    this.load.tilemapTiledJSON("map", "assets/map/map.json");
+
+    // our two characters
+    this.load.spritesheet("player", "assets/RPG_assets.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+  },
+
+  create: function() {
+    this.scene.start("WorldScene");
+    1;
+    var map = this.make.tilemap({ key: "map" });
+    var tiles = map.addTilesetImage("spritesheet", "tiles");
+
+    var grass = map.createStaticLayer("Grass", tiles, 0, 0);
+    var obstacles = map.createStaticLayer("Obstacles", tiles, 0, 0);
+    obstacles.setCollisionByExclusion([-1]);
+  }
+});
+
+var WorldScene = new Phaser.Class({
+  Extends: Phaser.Scene,
+
+  initialize: function WorldScene() {
+    Phaser.Scene.call(this, { key: "WorldScene" });
+  },
+  preload: function() {},
+  create: function() {
+    // create your world here
+  }
+});
+
+var config = {
+  type: Phaser.AUTO,
+  parent: "content",
+  width: 320,
+  height: 240,
+  zoom: 2,
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 }
+    }
+  },
+  scene: [BootScene, WorldScene]
+};
+var game = new Phaser.Game(config);
