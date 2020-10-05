954882aa2c7eab8688883f3069e0d5c6de38340f
Glitch (hello-webpage) <none>
2020-10-05 08:12:50 +0000

    🐫👡 Checkpoint ./script.js:1045018/3705

--- script.js
+++ script.js
@@ -279,6 +279,28 @@ var WorldScene = new Phaser.Class({
     // // add collider
     // this.physics.add.overlap(this.player, this.spawns, this.onMeetEnemy, false, this);
     //
+    
+    // Follower stuff
+    // Followers are a type of Phaser.Sprite
+    Follower.prototype = Object.create(Phaser.Sprite.prototype);
+    Follower.prototype.constructor = Follower;
+
+    Follower.prototype.update = function() {
+        // Calculate distance to target
+        var distance = this.game.math.distance(this.x, this.y, this.target.x, this.target.y);
+
+        // If the distance > MIN_DISTANCE then move
+        if (distance > this.MIN_DISTANCE) {
+            // Calculate the angle to the target
+            var rotation = this.game.math.angleBetween(this.x, this.y, this.target.x, this.target.y);
+
+            // Calculate velocity vector based on rotation and this.MAX_SPEED
+            this.body.velocity.x = Math.cos(rotation) * this.MAX_SPEED;
+            this.body.velocity.y = Math.sin(rotation) * this.MAX_SPEED;
+        } else {
+            this.body.velocity.setTo(0, 0);
+        }
+    };
   },
 
   update: function(time, delta) {
@@ -390,7 +412,30 @@ var WorldScene = new Phaser.Class({
       gameOptions.taha4Count = 0
     }
     
-  }
+  },
+  
+  // Follower constructor
+  Follower: function(game, x, y, target) {
+    
+    this.frisk = this.physics.add.sprite(200, 100, "enemies", 1);
+    Phaser.Sprite.call(this, game, x, y, 'player');
+
+    // Save the target that this Follower will follow
+    // The target is any object with x and y properties
+    this.target = target;
+
+    // Set the pivot point for this sprite to the center
+    this.anchor.setTo(0.5, 0.5);
+
+    // Enable physics on this object
+    this.game.physics.enable(this, Phaser.Physics.ARCADE);
+
+    // Define constants that affect motion
+    this.MAX_SPEED = 250; // pixels/second
+    this.MIN_DISTANCE = 32; // pixels
+}
+
+
     
   
     
